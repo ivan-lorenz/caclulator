4ff71469ea32fbaeb30c8eb3e790ffb08401310c
Ivan Lorenz <ivan.lorenz@scmspain.com>
2017-02-18 21:03:29 +0100

    First solution

--- src/main/scala/calculator/Calculator.scala
+++ src/main/scala/calculator/Calculator.scala
@@ -11,11 +11,24 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+    namedExpressions.map { case (k,v) =>
+      if (isCyclicDependency(k, v(), namedExpressions))
+        k -> Var(Double.NaN)
+      else
+        k -> Var(eval(v(), namedExpressions))
+    }
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    expr match {
+      case Literal(v) => v
+      case Ref(name) => references.get(name).map(e => eval(e(), references)).getOrElse(Double.NaN)
+      case Plus(e1, e2) => eval(e1, references) + eval(e2, references)
+      case Minus(e1, e2) => eval(e1, references) - eval(e2, references)
+      case Times(e1, e2) => eval(e1, references) * eval(e2, references)
+      case Divide(e1, e2) => eval(e1, references) / eval(e2, references)
+      case _ => Double.NaN
+    }
   }
 
   /** Get the Expr for a referenced variables.
@@ -27,6 +40,28 @@ object Calculator {
       Literal(Double.NaN)
     } { exprSignal =>
       exprSignal()
+    }
+  }
+
+  private def isCyclicDependency(name: String, expr: Expr, references: Map[String, Signal[Expr]]): Boolean = {
+    expr match {
+      case Ref(n) => isReferencingName(name, getReferenceExpr(n, references))
+      case Plus(e1, e2) => isCyclicDependency(name, e1, references) || isCyclicDependency(name, e2, references)
+      case Minus(e1, e2) => isCyclicDependency(name, e1, references) || isCyclicDependency(name, e2, references)
+      case Times(e1, e2) => isCyclicDependency(name, e1, references) || isCyclicDependency(name, e2, references)
+      case Divide(e1, e2) => isCyclicDependency(name, e1, references) || isCyclicDependency(name, e2, references)
+      case _ => false
+    }
+  }
+
+  private def isReferencingName(name: String, expr: Expr): Boolean = {
+    expr match {
+      case Ref(n) => n == name
+      case Plus(e1, e2) => isReferencingName(name, e1) || isReferencingName(name, e2)
+      case Minus(e1, e2) => isReferencingName(name, e1) || isReferencingName(name, e2)
+      case Times(e1, e2) => isReferencingName(name, e1) || isReferencingName(name, e2)
+      case Divide(e1, e2) => isReferencingName(name, e1) || isReferencingName(name, e2)
+      case _ => false
     }
   }
 }
--- src/main/scala/calculator/Polynomial.scala
+++ src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,17 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Var(scala.math.pow(b(), 2) - (4 * a() * c()))
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Var(delta() match {
+      case d if d < 0 => Set.empty[Double]
+      case d if d == 0 => Set(-b() / (2 * a()))
+      case _ =>
+        val sqrtDelta = scala.math.sqrt(delta())
+        Set.empty + (-b() + sqrtDelta) / (2 * a()) + (-b() - sqrtDelta) / (2 * a())
+    })
   }
 }
